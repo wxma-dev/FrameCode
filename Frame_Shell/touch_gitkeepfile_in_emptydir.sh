9882f2e4496b147dbe1f96f17dde086e1a886b37
wxma-dev <531747153@qq.com>
2020-06-14 16:41:48 +0800

    modified:   Frame_Shell/touch_gitkeepfile_in_emptydir.sh

--- Frame_Shell/touch_gitkeepfile_in_emptydir.sh
+++ Frame_Shell/touch_gitkeepfile_in_emptydir.sh
@@ -7,22 +7,48 @@ fi
 workdir=$1
 
 
-#touch file .gitkeep in emtpy dir
-find $workdir -type d -empty | xargs du -a | awk '{if ($1 == 0 ) {print $2}}' | while read line
+#echo in emtpy dir
+echo "----------------------------start----list empty dir--------------------------------"
+find $workdir -type d -empty | while read line
 do
-	echo "empty dir:$line"
-	touch ${line}/.gitkeep
+	echo "$line"
 done
+echo "-----------------------------end-----list empty dir--------------------------------"
+
+icount=`find $workdir -type d -empty | wc -l`
+echo "icount=[$icount]"
+if [ "$icount" -eq "0" ] ; then
+	echo "not have any empty dir"
+	exit
+fi
+
+
+#touch file .gitkeep in emtpy dir
+printf "\n\n\n"
+echo -n "are you sure touch .gitkeep in the empty dir(y/n)?"
+read line
+if [ "$line" == "y" ] ; then
+	find $workdir -type d -empty | while read line
+	do
+		touch ${line}/.gitkeep && echo "touch ${line}/.gitkeep success"
+	done
+else
+	exit
+fi
 
 #check empty dir
-count="0"
-find $workdir -type d -empty | xargs du -a | awk '{if ($1 == 0 ) {print $2}}' | while read line
+find $workdir -type d -empty | while read line
 do
-	count=`expr $count + 1`;
 	echo "check, hava empty dir :$line"
 done
 
+count=`find $workdir -type d -empty | wc -l`
+echo "count=[$count]"
 if [ "$count" -eq "0" ] ; then
-	echo "check, not have empty dir"
+	echo "check,count=[$count] not have empty dir"
 fi
+
+
+
+
 
